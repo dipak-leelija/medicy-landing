d8c84a499c6a3ddae6b1de19a8212b227d92979a
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-03-01 18:11:28 +0530

    footer section almost completed

--- src/componant/Footer.jsx
+++ src/componant/Footer.jsx
@@ -1,46 +1,133 @@
-import React from 'react'
-import logo from '/assets/img/logo.png';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Footer() {
-    return (
-        <>
-            <div className='bg-dark w-100 h-auto tect-center tex-white'>
-                <section className='d-flex justify-content-around flex-wrap'>
-                    <div className='w-25'>
-                        <img className='' src={logo}></img>
+  return (
+    <>
+      <div className="footer-frame">
+        <section className="footer-sec frame-about-us-careers">
+          <footer className="frame-important-text">
+            <div className="support-frame">
+              <div className="framework-about-us row justify-content-center pt-4">
+                <div className="footer-col-1 col-12 col-lg-5">
+                  <h2 className="footer-title">Contact Area</h2>
+                  <div className="footer-contact-box mt-4">
+                    <div className="social-box">
+                      <a href="#" rel="noopener noreferrer">
+                        <svg
+                          class="social-svg"
+                          style={{ marginBottom: "-5px" }}
+                          viewBox="0 0 67 67"
+                          xml:space="preserve"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                        >
+                          <path d="M29.765,50.32h6.744V33.998h4.499l0.596-5.624h-5.095  l0.007-2.816c0-1.466,0.14-2.253,2.244-2.253h2.812V17.68h-4.5c-5.405,0-7.307,2.729-7.307,7.317v3.377h-3.369v5.625h3.369V50.32z   M34,64C17.432,64,4,50.568,4,34C4,17.431,17.432,4,34,4s30,13.431,30,30C64,50.568,50.568,64,34,64z" />
+                        </svg>
+                      </a>
+
+                      <a href="#" rel="noopener noreferrer">
+                        <svg
+                          class="social-svg"
+                          viewBox="0 0 56.7 56.7"
+                          xml:space="preserve"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                        >
+                          <g>
+                            <circle cx="28.1" cy="30" r="4.4" />
+                            <path d="M33.6,19.2h-11c-1.6,0-3,0.5-3.9,1.4c-0.9,0.9-1.4,2.3-1.4,3.9v11c0,1.6,0.5,3,1.5,4c1,0.9,2.3,1.4,3.9,1.4h10.9   c1.6,0,3-0.5,3.9-1.4c1-0.9,1.5-2.3,1.5-3.9v-11c0-1.6-0.5-2.9-1.4-3.9C36.6,19.7,35.3,19.2,33.6,19.2z M28.1,36.8   c-3.8,0-6.8-3.1-6.8-6.8c0-3.8,3.1-6.8,6.8-6.8S35,26.2,35,30C35,33.8,31.9,36.8,28.1,36.8z M35.2,24.5c-0.9,0-1.6-0.7-1.6-1.6   s0.7-1.6,1.6-1.6s1.6,0.7,1.6,1.6S36.1,24.5,35.2,24.5z" />
+                            <path d="M28.3,5.2c-13.6,0-24.6,11-24.6,24.6c0,13.6,11,24.6,24.6,24.6c13.6,0,24.6-11,24.6-24.6C53,16.2,41.9,5.2,28.3,5.2z    M41.4,35.6c0,2.3-0.8,4.3-2.2,5.7c-1.4,1.4-3.4,2.1-5.6,2.1H22.7c-2.2,0-4.2-0.7-5.6-2.1c-1.5-1.4-2.2-3.4-2.2-5.7v-11   c0-4.6,3.1-7.8,7.8-7.8h11c2.3,0,4.2,0.8,5.6,2.2c1.4,1.4,2.1,3.3,2.1,5.6V35.6z" />
+                          </g>
+                        </svg>
+                      </a>
+                      <a href="#" rel="noopener noreferrer">
+                        <svg
+                          class="social-svg"
+                          viewBox="0 0 32 32"
+                          xml:space="preserve"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                        >
+                          <g id="OUTLINE_copy_2">
+                            <g>
+                              <path d="M16,0C7.164,0,0,7.163,0,16c0,8.836,7.164,16,16,16s16-7.164,16-16C32,7.163,24.836,0,16,0z M11.844,22.277H8.58v-9.82    h3.264V22.277z M10.212,11.116h-0.021c-1.096,0-1.804-0.755-1.804-1.697c0-0.963,0.73-1.696,1.846-1.696    c1.116,0,1.804,0.733,1.825,1.696C12.058,10.362,11.349,11.116,10.212,11.116z M23.613,22.277L23.613,22.277l-3.264,0v-5.254    c0-1.32-0.472-2.22-1.654-2.22c-0.901,0-1.438,0.607-1.674,1.194c-0.086,0.21-0.108,0.503-0.108,0.796v5.484H13.65    c0,0,0.043-8.898,0-9.82h3.264v1.391c0.433-0.669,1.209-1.622,2.942-1.622c2.147,0,3.757,1.404,3.757,4.42V22.277z" />
+                            </g>
+                          </g>
+                        </svg>
+                      </a>
+                      <a href="#" rel="noopener noreferrer">
+                        <svg
+                          class="social-svg"
+                          version="1.1"
+                          viewBox="0 0 512 512"
+                          width="100%"
+                          xml:space="preserve"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                        >
+                          <path d="M256,0c141.29,0 256,114.71 256,256c0,141.29 -114.71,256 -256,256c-141.29,0 -256,-114.71 -256,-256c0,-141.29 114.71,-256 256,-256Zm-45.091,392.158c113.283,0 175.224,-93.87 175.224,-175.223c0,-2.682 0,-5.364 -0.128,-7.919c12.005,-8.684 22.478,-19.54 30.779,-31.928c-10.983,4.853 -22.861,8.174 -35.377,9.706c12.772,-7.663 22.478,-19.668 27.076,-34.099c-11.878,7.024 -25.032,12.132 -39.081,14.942c-11.239,-12.005 -27.203,-19.412 -44.955,-19.412c-33.972,0 -61.558,27.586 -61.558,61.558c0,4.853 0.511,9.578 1.66,14.048c-51.213,-2.554 -96.552,-27.075 -126.947,-64.368c-5.237,9.068 -8.302,19.668 -8.302,30.907c0,21.328 10.856,40.23 27.459,51.213c-10.09,-0.255 -19.541,-3.065 -27.842,-7.662l0,0.766c0,29.885 21.2,54.661 49.425,60.409c-5.108,1.404 -10.6,2.171 -16.219,2.171c-3.96,0 -7.791,-0.383 -11.622,-1.15c7.79,24.521 30.523,42.274 57.471,42.784c-21.073,16.476 -47.637,26.31 -76.501,26.31c-4.981,0 -9.834,-0.256 -14.687,-0.894c26.948,17.624 59.387,27.841 94.125,27.841Z" />
+                        </svg>
+                      </a>
                     </div>
-                    <div className='d-flex w-50 text-white justify-content-around flex-wrap'>
-                        <div><h4>Customer</h4>
-                            <p>Refer a chemist friend</p>
-                            <p>FAQs</p>
-                            <p>FAQs</p>
-                            <p>FAQs</p>
+                    <div className="contact-box">
+                      <div className="contact">
+                        <div className="icon-bx">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M280 0C408.1 0 512 103.9 512 232c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-101.6-82.4-184-184-184c-13.3 0-24-10.7-24-24s10.7-24 24-24zm8 192a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm-32-72c0-13.3 10.7-24 24-24c75.1 0 136 60.9 136 136c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-48.6-39.4-88-88-88c-13.3 0-24-10.7-24-24zM117.5 1.4c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L144 207.3c33.3 70.4 90.3 127.4 160.7 160.7L345 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C481.8 499.9 466 512 448 512C200.6 512 0 311.4 0 64C0 46 12.1 30.2 29.5 25.4l88-24z"/>
+                            </svg>
                         </div>
-                        <div>
-                            <h4>Product</h4>
-                            <p>Refer a chemist friend</p>
-                            <p>FAQs</p>
-                            <p>FAQs</p>
-                            <p>FAQs</p>
+                        <div className="supportmedicyin2">
+                          <p className="m-0">7699753019</p>
+                          <p className="m-0">support@medicy.in</p>
                         </div>
-                        <div>
-                            <h4>About Us</h4>
-                            <p>Refer a chemist friend</p>
-                            <p>FAQs</p>
-                            <p>FAQs</p>
-                            <p>FAQs</p>
+                      </div>
+
+                      <div className="location">
+                        <div className="icon-bx">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 384 512"
+                          >
+                            <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+                          </svg>
                         </div>
+                        <div className="supportmedicyin2">
+                          <p className="m-0">7699753019</p>
+                          <p className="m-0">support@medicy.in</p>
+                        </div>
+                      </div>
                     </div>
-                </section>
-                <section className='w-100 text-white mt-5'>
-                    <div className='d-flex justify-content-around align-item-center'>
-                        <p>Privacy Policy</p>
-                        <p>Terms and conditions</p>
-                        <p>© 2023-2024 Medicy Health Care.</p>
-                    </div>
-                </section>
+                  </div>
+                </div>
+                <div className="footer-col-2 col-5 col-lg-2 col-xl-3 mt-3 mt-lg-0">
+                  <h2 className="footer-title">Important</h2>
+                  <div className="footer-list-item-box ps-1  mt-4">
+                    <a className="footer-list-item" href="/">About Us</a>
+                    <a className="footer-list-item" href="/">Careers</a>
+                    <a className="footer-list-item" href="/">Support</a>
+                    <a className="footer-list-item" href="/">Privacy Policy</a>
+                    <a className="footer-list-item" href="/">Refund Rules</a>
+                  </div>
+                </div>
+                <div className="footer-col-3 col-5 col-lg-2 col-xl-3 mt-3 mt-lg-0">
+                  <h2 className="footer-title">Company</h2>
+                  <div className="footer-list-item-box ps-1 mt-4">
+                    <a className="footer-list-item" href="/">FAQs</a>
+                    <a className="footer-list-item" href="/">Documentation</a>
+                    <a className="footer-list-item" href="/">Support</a>
+                    <a className="footer-list-item" href="/">Refund Policy</a>
+                    <a className="footer-list-item" href="/">Refund Rules</a>
+                  </div>
+                </div>
+              </div>
             </div>
-        </>
-    )
+            <p className="copyright-text small">
+              &copy; 2024 Medicy. All rights reserved.
+            </p>
+          </footer>
+        </section>
+      </div>
+    </>
+  );
 }
