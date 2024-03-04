8b5011181d4819b216f832601a21ffa1746fddd3
Jayanta Barman <jbleelija@gmail.com>
2024-03-04 18:35:00 +0530

    add new componant

---
+++ src/views/Chooseus.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+// import "bootstrap/dist/css/bootstrap.min.css";
+import chooseImg from '/assets/img/chooseus.png';
+
+export default function Chooseus() {
+    return (
+        <div className="wby-choose-us">
+            <div className="main-div">
+                <div className="frame">
+                    <div className="why-choose-us-wrapper">
+                        <b className="why-choose-us">Why Choose Us?</b>
+                    </div>
+                </div>
+                <div className="frame-parent ">
+                    <div className="frame1">
+                        <div className="frame2">
+                            <img
+                                className="removebg-preview-1-icon"
+                                alt=""
+                                src={chooseImg}
+                            />
+                        </div>
+                    </div>
+                        <div className="frame3">
+                            <div className="frame4">
+                                <div className="frame5">
+                                    <div className="cloud-based-software-wrapper">
+                                        <div className="cloud-based-software">
+                                            Cloud Based Software
+                                        </div>
+                                    </div>
+                                    <div className="be-always-connected-with-your-wrapper">
+                                        <div className="be-always-connected">
+                                            Be always connected with your Pharmacy with cloud
+                                            technology. Monitor your Medical store from anywhere
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="frame6">
+                                <div className="frame7">
+                                    <div className="cloud-based-software-wrapper">
+                                        <div className="cloud-based-software">
+                                            Cloud Based Software
+                                        </div>
+                                    </div>
+                                    <div className="be-always-connected-with-your-wrapper">
+                                        <div className="be-always-connected">
+                                            Be always connected with your Pharmacy with cloud
+                                            technology. Monitor your Medical store from anywhere
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="frame8">
+                            <div className="frame9">
+                                <div className="frame5">
+                                    <div className="cloud-based-software-wrapper">
+                                        <div className="cloud-based-software2">
+                                            Cloud Based Software
+                                        </div>
+                                    </div>
+                                    <div className="be-always-connected-with-your-frame">
+                                        <div className="be-always-connected">
+                                            Be always connected with your Pharmacy with cloud
+                                            technology. Monitor your Medical store from anywhere
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="frame11">
+                                <div className="frame7">
+                                    <div className="cloud-based-software-wrapper">
+                                        <div className="cloud-based-software3">
+                                            Cloud Based Software
+                                        </div>
+                                    </div>
+                                    <div className="be-always-connected-with-your-frame">
+                                        <div className="be-always-connected">
+                                            Be always connected with your Pharmacy with cloud
+                                            technology. Monitor your Medical store from anywhere
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
