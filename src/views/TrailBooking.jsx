f3e1ceebbc4fe901ace70bbbaab873c4d3f4b3b2
Jayanta Barman <jbleelija@gmail.com>
2024-03-06 15:28:08 +0530

    modify trailbokking componant and css page

--- src/views/TrailBooking.jsx
+++ src/views/TrailBooking.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function TrailBooking() {
   return (
-    <section className='main-trail'>
+    <div className='main-trail'>
         <div className='sub-trail'>
-            <div>Book Your Trail</div>
-            <div>Lorem Ipsum is simply dummy text of the  and typesetting industry. Lorem Ipsum has been the industry's standard dummys,  an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
-            <div>
-               <button>Get Demo</button>
+            <div className='trailhead'><h2>Book Your Trail</h2></div>
+            <div className='trail-text'>Lorem Ipsum is simply dummy text of the  and typesetting industry. Lorem Ipsum has been the industry's standard dummys,  an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+            <div className='trailButton'>
+               <button className='btn btn-primary text-white fw-bold rounded-pill tbtn'>Get Demo</button>
             </div>
         </div>
-    </section>
+    </div>
   )
 }
