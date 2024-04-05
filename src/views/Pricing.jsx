232a6fae166b438d7c50c2c94eaf2e8e6faf0373
Jayanta <jbleelija@gmail.com>
2024-04-05 17:42:30 +0530

    modify pricing page for style

--- src/views/Pricing.jsx
+++ src/views/Pricing.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import circleCheck from '../../public/assets/icon/circle-check.svg';
+import circleUnCheck from '../../public/assets/icon/circle-uncheck.svg';
 
 export default function Pricing() {
 	return (
@@ -6,44 +8,157 @@ export default function Pricing() {
 			<div className=' main-pricediv'>
 				<div className='row row-cols-1 row-cols-md-3 g-4'>
 					<div className='col'>
-						<div className='card'>
+						<div className='card price-card'>
 							<div className='card-body'>
-								<h5 className='card-title text-center p-4'>Standart</h5>
+								<div className='head text-center mb-4 p-4 pb-0'>
+									<h3 className='mb-0 fw-semibold'>Basic</h3>
+									<p className='text-center '>starter</p>
+								</div>
+
+								<div className='show-price text-center mb-4'>
+									<h4>
+										<label htmlFor=''>&#x20b9;</label>
+										<span className='fw-bold dark-primary'>3,500</span>/ year
+									</h4>
+								</div>
 
 								<div className='p-3'>
-									<div className='d-flex justify-content-between'>
-										<div>Lab Test</div>
-										<div>✔</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Lab Test</span>
+										<img src={circleCheck} alt='' />
 									</div>
-									<div className='d-flex justify-content-between'>
-										<div>Lab Test</div>
-										<div>✔</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Report</span>
+										<img src={circleCheck} alt='' />
 									</div>
-									<div className='d-flex justify-content-between'>
-										<div>Lab Test</div>
-										<div>✔</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Product</span>
+										<img src={circleUnCheck} alt='' />
 									</div>
-									<div className='d-flex justify-content-between'>
-										<div>Lab Test</div>
-										<div>✔</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Sale Management</span>
+										<img src={circleUnCheck} alt='' />
 									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Purchase Management</span>
+										<img src={circleUnCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Stock Details</span>
+										<img src={circleUnCheck} alt='' />
+									</div>
+								</div>
+
+								<div className='d-flex justify-content-center m-4'>
+									<button type='button' className='btn btn-primary price-btn'>
+										Choose Plan
+									</button>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className='col'>
-						<div className='card'>
+						<div className='card price-card'>
 							<div className='card-body'>
-								<h5 className='card-title text-center p-4'>Standart +</h5>
-								<p className='card-text'></p>
+								<div className='head text-center mb-4 p-4 pb-0'>
+									<h3 className='mb-0 fw-semibold'>Standard</h3>
+									<p className='text-center '>Better result</p>
+								</div>
+
+								<div className='show-price text-center mb-4'>
+									<h4>
+										<label htmlFor=''>&#x20b9;</label>
+										<span className='fw-bold dark-primary'>3,500</span>/ year
+									</h4>
+								</div>
+
+								<div className='p-3'>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Lab Test</span>
+										<img src={circleCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Report</span>
+										<img src={circleCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Product</span>
+										<img src={circleCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Sale Management</span>
+										<img src={circleUnCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Purchase Management</span>
+										<img src={circleUnCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Stock Details</span>
+										<img src={circleUnCheck} alt='' />
+									</div>
+								</div>
+
+								<div className='d-flex justify-content-center m-4'>
+									<button type='button' className='btn btn-primary price-btn'>
+										Choose Plan
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
 					<div className='col'>
-						<div className='card'>
+						<div className='card price-card'>
+							<div>
+								<div className='pricebdg text-center text-white p-1'>
+									popular
+								</div>
+							</div>
 							<div className='card-body'>
-								<h5 className='card-title text-center p-4'>Professional</h5>
-								<p className='card-text'></p>
+								<div className='head text-center mb-4 p-4 pb-0'>
+									<h3 className='mb-0 fw-semibold'>Premium</h3>
+									<p className='text-center '>Go all in one</p>
+								</div>
+
+								<div className='show-price text-center mb-4'>
+									<h4>
+										<label htmlFor=''>&#x20b9;</label>
+										<span className='fw-bold dark-primary'>3,500</span>/ year
+									</h4>
+								</div>
+
+								<div className='p-3'>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Lab Test</span>
+										<img src={circleCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Report</span>
+										<img src={circleCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Product</span>
+										<img src={circleCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Sale Management</span>
+										<img src={circleCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Purchase Management</span>
+										<img src={circleCheck} alt='' />
+									</div>
+									<div className='feature-head d-flex justify-content-between align-items-center mb-3'>
+										<span>Stock Details</span>
+										<img src={circleCheck} alt='' />
+									</div>
+								</div>
+
+								<div className='d-flex justify-content-center m-4'>
+									<button type='button' className='btn btn-primary price-btn'>
+										Choose Plan
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
