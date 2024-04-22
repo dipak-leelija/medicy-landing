b7001c8b87e3d738566117e15c713147ca7004c3
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-04-22 14:31:44 +0530

    spinner icon component created

---
+++ src/icons/MEspinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../public/assets/css/me-icons.css'
+
+function MEspinner() {
+  return (
+    // <div className="me-spinner-container">
+      <div className="me-spinner"></div>
+    // </div>
+  );
+}
+
+export default MEspinner;
