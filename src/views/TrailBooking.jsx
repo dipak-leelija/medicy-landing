a289c74e98f6d79f92e437224316c15b39bce818
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-04-24 17:44:55 +0530

    trail booking action section updated

--- src/views/TrailBooking.jsx
+++ src/views/TrailBooking.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function TrailBooking() {
   return (
-    <div className='main-trail'>
-        <div className='sub-trail'>
-            <div className='trailhead'><h1>Book Your FREE Trail</h1></div>
-            <div className='trail-text'>Talk to an expert today and learn how Medicy can help your business</div>
-            <div className='trailButton'>
-               <a href="" className='btn btn-primary text-white fw-bold  btn-lg tbtn'>Get Demo</a>
-            </div>
+    <section className="px-4">
+      <div className="main-trail">
+        <div className="sub-trail">
+          <div className="trailhead">
+            <h2>Book Your Free Trail</h2>
+          </div>
+          <div className="trail-text">
+            Talk to an expert today and learn how Medicy can help your business
+          </div>
+          <div className="trailButton">
+            <a
+              href=""
+              className="btn btn-primary text-white fw-bold  btn-lg tbtn"
+            >
+              Get Demo
+            </a>
+          </div>
         </div>
-    </div>
-  )
+      </div>
+    </section>
+  );
 }
