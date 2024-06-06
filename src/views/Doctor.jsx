4159650d0d943441d68f04097b11bc9c2961cfcf
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-06-06 17:15:58 +0530

    Doctor Page Needs to Update

--- src/views/Doctor.jsx
+++ src/views/Doctor.jsx
@@ -1,42 +1,52 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import doctor from '../../public/assets/img/doctor.png';
-import circleCheck from '/assets/icon/circle-check.svg';
+import React from "react";
+import { motion } from "framer-motion";
+import doctor from "../../public/assets/img/doctor.png";
+import circleCheck from "/assets/icon/circle-check.svg";
 export default function Doctor() {
-	return (
-		<section>
-			<motion.div
-				initial={{ opacity: 0, y: 50 }}
-				whileInView={{ opacity: 1, y: 0 }}
-				transition={{ duration: 0.5, delay: 0.4 }}
-				viewport={{ once: true }}
-				className='d-flex justify-content-between doctor-banner '>
-				<div className='doc-shape1'></div>
-				<div className='doc-shape2'></div>
-				<div className='doc-shape3'></div>
-				<div className='doc-shape4'></div>
-				<div className='doc-shape5'></div>
+  return (
+    <section>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        viewport={{ once: true }}
+        className="d-flex justify-content-between doctor-banner "
+      >
+        <div className="doc-shape1"></div>
+        <div className="doc-shape2"></div>
+        <div className="doc-shape3"></div>
+        <div className="doc-shape4"></div>
+        <div className="doc-shape5"></div>
 
-				<div className='doc-Leftdiv'>
-					<div>
-						<h1>Discover Our Doctor Service Today !</h1>
-					</div>
-					<div className='mb-2 doc-feat fs-5'>
-						<img src={circleCheck} alt='' className='me-3' /> <span>searching</span>
-					</div>
-					<div className='mb-2 doc-feat fs-5'>
-						<img src={circleCheck} alt='' className='me-3' /> <span>pharmacy</span>
-					</div>
-					<div className='mb-2 doc-feat fs-5'>
-						<img src={circleCheck} alt='' className='me-3' /> <span>pharmacy</span>
-					</div>
-				</div>
-				<div className='d-flex justify-content-end'>
-					<div className='doctor-img'>
-						<img src={doctor} alt='' />
-					</div>
-				</div>
-			</motion.div>
-		</section>
-	);
+        <div className="doc-Leftdiv">
+			<span>Under Construction</span>
+          <h2 className="title">What You Can Look Forward To</h2>
+
+          <div className="mb-2 doc-feat">
+            <img src={circleCheck} alt="" className="me-3" />{" "}
+            <span><strong>Search by Location:</strong> Easily find doctors near you based on your location.</span>
+          </div>
+          <div className="mb-2 doc-feat">
+            <img src={circleCheck} alt="" className="me-3" />{" "}
+            <span><strong>Filter by Specialty:</strong> Look for doctors by their designation and specialty.</span>
+          </div>
+          <div className="mb-2 doc-feat">
+            <img src={circleCheck} alt="" className="me-3" />{" "}
+            <span><strong>View Detailed Information:</strong> Access addresses, contact details, and available hours.</span>
+          </div>
+		  <div className="mb-2 doc-feat">
+            <img src={circleCheck} alt="" className="me-3" />{" "}
+            <span><strong>Real-Time Availability:</strong> Check when doctors are available and book appointments.</span>
+          </div>
+		  <p>Stay tuned for more updates and get ready to find the best healthcare providers in your area!</p>
+        </div>
+		
+        <div className="d-flex justify-content-end">
+          <div className="doctor-img">
+            <img src={doctor} alt="" />
+          </div>
+        </div>
+      </motion.div>
+    </section>
+  );
 }
